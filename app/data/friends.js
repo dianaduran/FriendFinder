ed7640c91bafd0e0a4896a6b8add91a3049f8320
Diana Duran <dianaduran17@gmail.com>
2018-04-23 18:20:44 -0400

    footer

---
+++ app/data/friends.js
@@ -0,0 +1,68 @@
+var Masculines = [{
+        "name": "Romeo Santos",
+        "photo": "https://812f0a29-a-62cb3a1a-s-sites.googlegroups.com/site/inforcororugama/home/bachata/cantantes/ROMEO.jpg?attachauth=ANoY7cpG3vgOBBFFm3kroDk6N96ZxTj0jRI8DJVF_zq1TuDuzr0Dc47uE23aarB9xlCm8byv4jA8gbtQHHEE6ZGyxo4ydAlpBd3ngrrCE_gt5X-nPxMiax4gvp1xQIRgm6glp8lQAdUdPWvew81TWutpQqqeVJDaTrF-qyGBecCGPn2C60iDjZnOC2yb7SEqvsIXZsSHf6wVjRzO3ghpaNDxdo-FP12WWLOoX3eEX6uvBLEwbMDFGOlSZJj00NYbUU32Qhl80zz2&attredirects=0",
+        "scores": [
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ]
+    },
+    {
+        "name": "PRINCE ROYCE",
+        "photo": "https://812f0a29-a-62cb3a1a-s-sites.googlegroups.com/site/inforcororugama/home/bachata/cantantes/images.jpg?attachauth=ANoY7cobxjip8RdixUWeuf5b74KjT7vYSDIp4QaUDpM8AvOwVDFctUw6MabM9i6bIq1AWcBqPaSy1UBHnAk-DndbeaP-iROm_qft0xyoZwonFVnoY2C6qcEztVcjuwHyTSpxHC8W3yKaJdcAA_EUeEan79eUUNrhYm0Exk9JyL4sKz6vtfiqgy55HB_OHRPf3TEVIJnm7aRj-qvsxs9p95Q-_rnAJUQHbgocOUPClvD7j4mT9PLd42lfCsH803qc-v1uWQlAinp0&attredirects=0",
+        "scores": [
+            4,
+            2,
+            3,
+            5,
+            4,
+            2,
+            3,
+            4,
+            3,
+            1
+        ]
+    },
+
+    {
+        "name": "Enrique Iglesias",
+        "photo": "http://www.beautyzion.com/wp-content/uploads/2014/03/Enrique-Iglesias.jpg",
+        "scores": [
+            3,
+            1,
+            2,
+            4,
+            3,
+            1,
+            2,
+            3,
+            2,
+            2
+        ]
+    },
+
+    {
+        "name": "Enrique Iglesias",
+        "photo": "http://www.beautyzion.com/wp-content/uploads/2014/03/Enrique-Iglesias.jpg",
+        "scores": [
+            3,
+            1,
+            2,
+            4,
+            3,
+            1,
+            2,
+            3,
+            2,
+            2
+        ]
+    }
+
+];
