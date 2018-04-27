cd98e1bd233fc6a9f3e4b28d0976f4bd2ccbea26
Diana Duran <dianaduran17@gmail.com>
2018-04-26 20:23:35 -0400

    calculate

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,139 +1,138 @@
-var API = [{
-        "name": "Romeo Santos",
-        "sex": "Masculine",
-        "photo": "https://812f0a29-a-62cb3a1a-s-sites.googlegroups.com/site/inforcororugama/home/bachata/cantantes/ROMEO.jpg?attachauth=ANoY7cpG3vgOBBFFm3kroDk6N96ZxTj0jRI8DJVF_zq1TuDuzr0Dc47uE23aarB9xlCm8byv4jA8gbtQHHEE6ZGyxo4ydAlpBd3ngrrCE_gt5X-nPxMiax4gvp1xQIRgm6glp8lQAdUdPWvew81TWutpQqqeVJDaTrF-qyGBecCGPn2C60iDjZnOC2yb7SEqvsIXZsSHf6wVjRzO3ghpaNDxdo-FP12WWLOoX3eEX6uvBLEwbMDFGOlSZJj00NYbUU32Qhl80zz2&attredirects=0",
-        "scores": [
-            5,
-            1,
-            4,
-            4,
-            5,
-            1,
-            2,
-            5,
-            4,
-            1
-        ]
-    },
-    {
-        "name": "PRINCE ROYCE",
-        "sex": "Masculine",
-        "photo": "https://812f0a29-a-62cb3a1a-s-sites.googlegroups.com/site/inforcororugama/home/bachata/cantantes/images.jpg?attachauth=ANoY7cobxjip8RdixUWeuf5b74KjT7vYSDIp4QaUDpM8AvOwVDFctUw6MabM9i6bIq1AWcBqPaSy1UBHnAk-DndbeaP-iROm_qft0xyoZwonFVnoY2C6qcEztVcjuwHyTSpxHC8W3yKaJdcAA_EUeEan79eUUNrhYm0Exk9JyL4sKz6vtfiqgy55HB_OHRPf3TEVIJnm7aRj-qvsxs9p95Q-_rnAJUQHbgocOUPClvD7j4mT9PLd42lfCsH803qc-v1uWQlAinp0&attredirects=0",
-        "scores": [
-            4,
-            2,
-            3,
-            5,
-            4,
-            2,
-            3,
-            4,
-            3,
-            1
-        ]
-    },
-    {
-        "name": "Enrique Iglesias",
-        "sex": "Masculine",
-        "photo": "http://www.beautyzion.com/wp-content/uploads/2014/03/Enrique-Iglesias.jpg",
-        "scores": [
-            3,
-            1,
-            2,
-            4,
-            3,
-            1,
-            2,
-            3,
-            2,
-            2
-        ]
-    },
-    {
-        "name": "Ricardo Arjona",
-        "sex": "Masculine",
-        "photo": "http://arjoneando.com/wp-content/uploads/2015/02/Ricardo-Arjona.jpg",
-        "scores": [
-            1,
-            2,
-            3,
-            4,
-            5,
-            7,
-            8,
-            9,
-            3,
-            2
-        ]
-    },
-    {
-        "name": "Jennifer Lopez",
-        "sex": "Female",
-        "photo": "https://en.wikipedia.org/wiki/Jennifer_Lopez#/media/File:Jennifer_Lopez_at_GLAAD_Media_Awards_(cropped).jpg",
-        "scores": [
-            1,
-            2,
-            3,
-            4,
-            5,
-            7,
-            8,
-            9,
-            3,
-            2
-        ]
-    },
-    {
-        "name": "Shakira",
-        "sex": "Female",
-        "photo": "https://en.wikipedia.org/wiki/Shakira#/media/File:Shakira_-_Live_Paris_-_2010_(1).jpg",
-        "scores": [
-            3,
-            4,
-            5,
-            7,
-            7,
-            9,
-            7,
-            8,
-            1,
-            3
-        ]
-    },
-    {
-        "name": "Britney Spears",
-        "sex": "Female",
-        "photo": "https://en.wikipedia.org/wiki/Britney_Spears#/media/File:Britney_Spears_2013_(Straighten_Crop).jpg",
-        "scores": [
-            1,
-            3,
-            4,
-            6,
-            8,
-            1,
-            5,
-            7,
-            3,
-            4
-        ]
-    },
-    {
-        "name": "Selena Gomez",
-        "sex": "Female",
-        "photo": "https://en.wikipedia.org/wiki/Selena_Gomez#/media/File:Selena_Gomez_-_Walmart_Soundcheck_Concert.jpg",
-        "scores": [
-            2,
-            9,
-            5,
-            7,
-            8,
-            2,
-            6,
-            3,
-            1,
-            5
-        ]
-    }
-]
+var API = {
+    "men": [{
+            "name": "Romeo Santos",
+            "photo": "https://812f0a29-a-62cb3a1a-s-sites.googlegroups.com/site/inforcororugama/home/bachata/cantantes/ROMEO.jpg?attachauth=ANoY7cpG3vgOBBFFm3kroDk6N96ZxTj0jRI8DJVF_zq1TuDuzr0Dc47uE23aarB9xlCm8byv4jA8gbtQHHEE6ZGyxo4ydAlpBd3ngrrCE_gt5X-nPxMiax4gvp1xQIRgm6glp8lQAdUdPWvew81TWutpQqqeVJDaTrF-qyGBecCGPn2C60iDjZnOC2yb7SEqvsIXZsSHf6wVjRzO3ghpaNDxdo-FP12WWLOoX3eEX6uvBLEwbMDFGOlSZJj00NYbUU32Qhl80zz2&attredirects=0",
+            "scores": [
+                5,
+                1,
+                4,
+                4,
+                5,
+                1,
+                2,
+                5,
+                4,
+                1
+            ]
+        },
+        {
+            "name": "PRINCE ROYCE",
+            "photo": "https://812f0a29-a-62cb3a1a-s-sites.googlegroups.com/site/inforcororugama/home/bachata/cantantes/images.jpg?attachauth=ANoY7cobxjip8RdixUWeuf5b74KjT7vYSDIp4QaUDpM8AvOwVDFctUw6MabM9i6bIq1AWcBqPaSy1UBHnAk-DndbeaP-iROm_qft0xyoZwonFVnoY2C6qcEztVcjuwHyTSpxHC8W3yKaJdcAA_EUeEan79eUUNrhYm0Exk9JyL4sKz6vtfiqgy55HB_OHRPf3TEVIJnm7aRj-qvsxs9p95Q-_rnAJUQHbgocOUPClvD7j4mT9PLd42lfCsH803qc-v1uWQlAinp0&attredirects=0",
+            "scores": [
+                4,
+                2,
+                3,
+                5,
+                4,
+                2,
+                3,
+                4,
+                3,
+                1
+            ]
+        },
+        {
+            "name": "Enrique Iglesias",
+            "photo": "http://www.beautyzion.com/wp-content/uploads/2014/03/Enrique-Iglesias.jpg",
+            "scores": [
+                3,
+                1,
+                2,
+                4,
+                3,
+                1,
+                2,
+                3,
+                2,
+                2
+            ]
+        },
+        {
+            "name": "Ricardo Arjona",
+            "sex": "Masculine",
+            "photo": "http://arjoneando.com/wp-content/uploads/2015/02/Ricardo-Arjona.jpg",
+            "scores": [
+                1,
+                2,
+                3,
+                4,
+                5,
+                1,
+                3,
+                4,
+                5,
+                2
+            ]
+        }
+    ],
+    "women": [{
+            "name": "Jennifer Lopez",
+            "photo": "https://en.wikipedia.org/wiki/Jennifer_Lopez#/media/File:Jennifer_Lopez_at_GLAAD_Media_Awards_(cropped).jpg",
+            "scores": [
+                1,
+                2,
+                3,
+                4,
+                5,
+                1,
+                3,
+                2,
+                1,
+                3
+            ]
+        },
+        {
+            "name": "Shakira",
+            "photo": "https://en.wikipedia.org/wiki/Shakira#/media/File:Shakira_-_Live_Paris_-_2010_(1).jpg",
+            "scores": [
+                3,
+                4,
+                5,
+                1,
+                3,
+                1,
+                2,
+                5,
+                1,
+                3
+            ]
+        },
+        {
+            "name": "Britney Spears",
+            "sex": "Female",
+            "photo": "https://en.wikipedia.org/wiki/Britney_Spears#/media/File:Britney_Spears_2013_(Straighten_Crop).jpg",
+            "scores": [
+                1,
+                3,
+                4,
+                3,
+                5,
+                1,
+                4,
+                3,
+                3,
+                4
+            ]
+        },
+        {
+            "name": "Selena Gomez",
+            "photo": "https://en.wikipedia.org/wiki/Selena_Gomez#/media/File:Selena_Gomez_-_Walmart_Soundcheck_Concert.jpg",
+            "scores": [
+                2,
+                1,
+                5,
+                2,
+                4,
+                2,
+                1,
+                3,
+                1,
+                5
+            ]
+        }
+    ]
+}
+
+
 
 module.exports = API;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -5,6 +5,7 @@ var API = function(app) {
     app.get('/api/friends', function(req, res) {
         res.json(apiFriends);
     });
+    // apiFriends.man[0].name
 
     // Sets up the Express app to handle data parsing
     // =============================================================
@@ -13,9 +14,45 @@ var API = function(app) {
 
     app.post('/api/friends', function(req, res) {
         var survey = req.body;
-        console.log(survey);
-        res.json(survey);
+        //console.log(survey);
+        var famous;
+
+        if (survey.sex == "F") {
+            var apiWoman = apiFriends.women;
+            var leastAmount = GetValue(survey.scores, apiWoman[0].scores);
+            famous = apiWoman[0];
+            for (var i = 1; i < apiWoman.length; i++) {
+                var aux = GetValue(survey.scores, apiWoman[i].scores);
+                if (leastAmount > aux) {
+                    leastAmount = aux;
+                    famous = apiWoman[i];
+                }
+            }
+        } else {
+            var apiMen = apiFriends.men;
+            var leastAmount = GetValue(survey.scores, apiMen[0].scores);
+            famous = apiMen[0];
+            for (var i = 1; i < apiMen.length; i++) {
+                var aux = GetValue(survey.scores, apiMen[i].scores);
+                if (leastAmount > aux) {
+                    leastAmount = aux;
+                    famous = apiMen[i];
+                }
+            }
+        }
+        console.log(famous);
+        res.json(famous);
     });
 };
 
 module.exports = API;
+
+function GetValue(score1, score2) {
+    var result = 0;
+    for (var i = 0; i < 10; i++) {
+        result += Math.abs(score1[i] - score2[i]);
+    }
+    console.log(result);
+    return result;
+
+}
